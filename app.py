18ba5216036cb5eef8f26fd0fc4e8a6904953258
RoelandKramer <r.s.kramer_1@tilburguniversity.edu>
2026-02-10 12:53:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,8 @@ class AuthConfig:
 class ApiConfig:
     base_url: str
 
+st.write("Secrets keys:", list(st.secrets.keys()))
+st.write("Has auth?", "auth" in st.secrets)
 
 # ----------------------------
 # Utilities
